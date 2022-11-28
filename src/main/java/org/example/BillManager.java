dabafcff814bb0c47c97072abcd8c5b2ec765282
h7359841 <h7359841@gmail.com>
2022-11-28 14:19:53 +0900

    update Bill, BillManager

--- src/main/java/org/example/BillManager.java
+++ src/main/java/org/example/BillManager.java
@@ -10,7 +10,7 @@ public class BillManager {
         for ( Bill bill : bills ) {
             if(bill.getAccountNum().equals(accountNum) )
             {
-                System.out.println(String.format("은행 : %s, 계좌번호 : %s, 입/출금 여부 : %s, 거래 금액 : %d, 거래일자 : %s, 거래 시간  : %s"  ,
+                System.out.println(String.format("은행 : %s, 계좌번호 : %s, 입/출금 여부 : %s, 거래 금액 : ￦%,d, 거래일자 : %s, 거래 시간  : %s"  ,
                         bill.getBankName(),bill.getAccountNum(),bill.getDepositOrWithdraw(),bill.getDealAmount(),bill.getDealDate(),bill.getDealTime()));
             }
 
