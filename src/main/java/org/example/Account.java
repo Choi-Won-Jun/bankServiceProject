c74d2be3f070807d5b9c3aefa2f36759f0ca25a1
choiwonjun <wonjon0930@khu.ac.kr>
2022-11-28 13:49:31 +0900

    Error in Account.java is fixed.

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import BillManager;
-import Bill;
+
 
 import java.util.Scanner;
 
@@ -13,51 +12,44 @@ public class Account {
 
 	
 	// ( 요구사항 7-1 ) 계좌는 입기능이 있다.
-	public void deposit (int amount) { 
-		
-		System.out.println("입금할 금액을 입력하세요.");
-		int amount = scanner.nextInt();
+	public void deposit(int amount) {
+
 		this.balance += amount;
+
 		System.out.println("입금이 완료되었습니다.");
-		
+
+		// 거래내역 생성
 		Bill bill = new Bill();
-		Bill.recordBill(bill);	
-		
-			// ( 요구사항 9 ) 계좌에서 잔고의 변화가 있을 때마다 거래 내역에 기록된다.
+
+		// ( 요구사항 9 ) 계좌에서 잔고의 변화가 있을 때마다 거래 내역에 기록된다.
 		BillManager billManager = new BillManager(); 
 		billManager.recordBill(bill);
-		
+	}
 	// ( 요구사항 7-2 ) 계좌는 출금 기능이 있다.
-	public void withdraw (int amount) {
-		
-		System.out.println("출금할 금액을 입력하세요.");
-		int amount = scanner.nextInt();
-		
+	public void withdraw(int amount) {
+
 		if (amount <= balance) {
 			this.balance -= amount;
-			
+
+			Bill bill = new Bill();
+
 			// ( 요구사항 9 ) 계좌에서 잔고의 변화가 있을 때마다 거래 내역에 기록된다.
-			BillManager billManager = new BillManager();
-			billManager.recordBill(bill);}
-		else {
-		System.out.println("계좌의 잔고가 부족합니다.");
-		} break;
+			BillManager.recordBill(bill);
 		}
-	
-	// ( 요구사항 11 ) 계좌는 모든 거래 내역을 조회할 수 있다. 	
-	public void getAllBills(accountNum) {
-		BillManager billManager = new BillManager();
-		billManager.getAllBills(accountNum);
+		else {
+			System.out.println("계좌의 잔고가 부족합니다.");
+		}
 	}
 	
-	
+	// ( 요구사항 11 ) 계좌는 모든 거래 내역을 조회할 수 있다. 	
+	public void getAllBills() {
+		BillManager.getAllBills(accountNum);
 	}
 	
 	// Getter
 	public String getOwnerName() {
 		return this.ownerName;
 	}
-	
 	public String getAccountNum() {
 		return this.accountNum;
 	}
--- src/main/java/org/example/BillManager.java
+++ src/main/java/org/example/BillManager.java
@@ -6,13 +6,13 @@ public class BillManager {
 
     private static ArrayList<Bill> bills = new ArrayList<>();
 
-    public void getAllBills(String accountNum) {
+    public static void getAllBills(String accountNum) {
         for ( Bill bill : bills ) {
             System.out.println(/* 내용 출력 로직 */);
         }
     }
 
-    public void recordBill(Bill bill){
+    public static void recordBill(Bill bill){
         bills.add(bill);
     }
 
